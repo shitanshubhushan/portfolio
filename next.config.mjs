f10f391b70adff1fcca810b6f41076c7c02e1a09
Shitanshu Bhushan <shitanshu273@gmail.com>
2025-01-14 03:47:17 -0500

    Fix Next.js config for GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-};
+  output: 'export',
+  basePath: '',
+  images: {
+    unoptimized: true,
+  },
+  experimental: {
+    appDir: true,
+  },
+}
 
 export default nextConfig; 
